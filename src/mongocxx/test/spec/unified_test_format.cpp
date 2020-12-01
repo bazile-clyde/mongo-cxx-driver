c0aa7bb62276b342d26667589312d7b457ab959e
Clyde Bazile <bazileclyde@gmail.com>
2020-11-30 19:21:53 -0500

    descriptions as sections

--- src/mongocxx/test/spec/unified_test_format.cpp
+++ src/mongocxx/test/spec/unified_test_format.cpp
@@ -12,9 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <bsoncxx/stdx/optional.hpp>
 #include <bsoncxx/test_util/catch.hh>
+#include <fstream>
+#include <mongocxx/instance.hpp>
 #include <mongocxx/test/spec/test_runner.hh>
 #include <mongocxx/test/spec/util.hh>
+#include <mongocxx/test_util/client_helpers.hh>
 
 namespace {
 
@@ -22,10 +26,32 @@ using namespace mongocxx;
 using namespace spec;
 
 void _run_unified_format_tests_in_file(std::string test_path) {
-    std::cout << "TEST PATH: " << test_path << std::endl;
+    using bsoncxx::v_noabi::stdx::optional;
+
+    // parse test file #############################################################################
+    INFO("Test path: " << test_path);
+    optional<document::value> test_spec = test_util::parse_test_file(test_path);
+    REQUIRE(test_spec);
+
+    std::cout << bsoncxx::to_json(test_spec->view()) << std::endl;
+    // ############################################################################################
+    auto test_spec_view = test_spec->view();
+    std::string description = test_spec_view["description"].get_string().value.to_string();
+    SECTION(description) {}
 }
 
 TEST_CASE("unified format spec automated tests", "[unified_format_spec]") {
-    run_tests_in_suite("UNIFIED_FORMAT_TESTS_PATH", _run_unified_format_tests_in_file);
+    instance::current();
+
+    std::string path = std::getenv("UNIFIED_FORMAT_TESTS_PATH");
+    REQUIRE(path.size());
+
+    std::ifstream test_files{path + "/test_files.txt"};
+    REQUIRE(test_files.good());
+
+    std::string test_file;
+    while (std::getline(test_files, test_file)) {
+        _run_unified_format_tests_in_file(path + "/" + test_file);
+    }
 }
 }
