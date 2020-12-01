276606180f01415f98ddcdc0d3c584c84533d389
Clyde Bazile <bazileclyde@gmail.com>
2020-11-30 20:21:50 -0500

    add schema validation

--- src/mongocxx/test/spec/unified_test_format.cpp
+++ src/mongocxx/test/spec/unified_test_format.cpp
@@ -19,23 +19,45 @@
 #include <mongocxx/test/spec/test_runner.hh>
 #include <mongocxx/test/spec/util.hh>
 #include <mongocxx/test_util/client_helpers.hh>
+#include <mongocxx/test_util/client_helpers.hh>
+#include <regex>
 
 namespace {
 
 using namespace mongocxx;
 using namespace spec;
 
+constexpr int runner_version[3] = {1, 0, 0};
+
 void _run_unified_format_tests_in_file(std::string test_path) {
     using bsoncxx::v_noabi::stdx::optional;
 
     // parse test file #############################################################################
-    INFO("Test path: " << test_path);
     optional<document::value> test_spec = test_util::parse_test_file(test_path);
     REQUIRE(test_spec);
-
-    std::cout << bsoncxx::to_json(test_spec->view()) << std::endl;
-    // ############################################################################################
     auto test_spec_view = test_spec->view();
+
+    // schemaVersion. required #####################################################################
+    const std::string schema = test_spec_view["schemaVersion"].get_string().value.to_string();
+
+    std::regex period("\\.");
+    std::vector<std::string> version_string = {
+        std::sregex_token_iterator(begin(schema), end(schema), period, -1),
+        std::sregex_token_iterator()};
+
+    std::vector<int> test_version;
+    std::transform(begin(version_string),
+                   end(version_string),
+                   std::back_inserter(test_version),
+                   [](const std::string s) { return std::stoi(s); });
+
+    enum v { k_major, k_minor, k_patch };
+    bool compatible = test_version[v::k_major] == runner_version[v::k_major] &&
+                      test_version[v::k_minor] <= runner_version[v::k_minor];
+    if (!compatible)
+        return;
+
+    // description. required #######################################################################
     std::string description = test_spec_view["description"].get_string().value.to_string();
     SECTION(description) {}
 }
